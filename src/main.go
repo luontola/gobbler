cd65acfd59440292f3790080794190ef52ddadf9
Esko Luontola <esko.luontola@gmail.com>
2010-02-04 15:02:00 +0200

    Build infrastructure

---
+++ src/main.go
@@ -0,0 +1,14 @@
+// Copyright © 2010 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+	"gobbler"
+)
+
+func main() {
+	gobbler.SayHello()
+}
+
